714197c052fa98c58b11b9c0f0c03341ddce1e1d
yhao2014 <https://github.com/>
2016-01-11 09:45:33 +0800

    修改代码

--- ckooc-ml/src/test/scala/basicstatistics/CorrelationsDemo.scala
+++ ckooc-ml/src/test/scala/basicstatistics/CorrelationsDemo.scala
@@ -1,8 +1,32 @@
 package basicstatistics
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * Created by yhao on 2016/1/10.
  */
-object CorrelationsDemo {
+object CorrelationsDemo extends App {
+  Logger.getRootLogger.setLevel(Level.WARN)
 
+  val conf = new SparkConf().setAppName("Correlations").setMaster("local[2]")
+  val sc = new SparkContext(conf)
+
+  val x = Seq(2.5, 4.0, 0.56)
+  val y = Seq(5.0, 8.0, 1.12)
+
+  val seriesX = sc.parallelize(x)
+  val seriesY = sc.parallelize(y)
+
+  val correlation = Statistics.corr(seriesX, seriesY, "pearson")
+  println(correlation)
+
+  val data = sc.parallelize(Seq(Vectors.dense(x.toArray), Vectors.dense(y.toArray)))
+
+  val correlMatrix = Statistics.corr(data)
+  println(correlMatrix.toString())
+
+  sc.stop()
 }
---
+++ ckooc-ml/src/test/scala/basicstatistics/StratifiedSamplingDemo.scala
@@ -0,0 +1,23 @@
+package basicstatistics
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * 分层采样示例
+ * Created by yhao on 2016/1/10.
+ */
+object StratifiedSamplingDemo extends App {
+  Logger.getRootLogger.setLevel(Level.WARN)
+
+  val conf = new SparkConf().setAppName("Correlations").setMaster("local[2]")
+  val sc = new SparkContext(conf)
+
+  val data = sc.parallelize(Seq(("yhao", 5.0), ("abc", 3.2)))
+  val fraction = Map(("yhao", 0.4), ("abc", 0.6))
+
+  val approxSample = data.sampleByKey(withReplacement = false, fraction)
+  val exactSample = data.sampleByKeyExact(withReplacement = false, fraction)
+
+  sc.stop()
+}
