d786fcfde9a7f2cf5d0db6e7c487126f6074ae5b
yhao2014 <https://github.com/>
2016-01-20 18:01:36 +0800

    更新LDA代码

--- ckooc-ml/src/main/scala/clustering/lda/LDAUtils.scala
+++ ckooc-ml/src/main/scala/clustering/lda/LDAUtils.scala
@@ -1,41 +1,44 @@
 package clustering.lda
 
+import java.io.File
+
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.{CountVectorizerModel, CountVectorizer, StopWordsRemover, RegexTokenizer}
+import org.apache.spark.mllib.clustering._
 import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
 /**
-  * Created by yhao on 2016/1/14.
+  * Created by yhao on 2016/1/20.
   */
 class LDAUtils {
 
 }
 
 object LDAUtils {
+
   /**
-    * 加载文件, 分词, 创建词汇表, 并将文件准备为term-counts的向量.
-    * @return (corpus, vocabulary as array, total token count in corpus)
-    *         <p>语料（由文档id以及文档的词汇表大小、词索引、词频组成的稀疏向量组成<p>词汇表（数组）<p>语料中的词数
+    * 预处理，读取数据文件，切分，去除停用词
+    * @param sc SparkContext
+    * @param paths  数据路径
+    * @param vocabSize  词汇表大小
+    * @param stopwordFile 停用词文件路径
+    * @return (corpus, vocabArray, actualNumTokens)
     */
   def preprocess(
-                          sc: SparkContext,
-                          paths: Seq[String],
-                          vocabSize: Int,
-                          stopwordFile: String): (RDD[(Long, Vector)], Array[String], Long) = {
+                  sc: SparkContext,
+                  paths: Seq[String],
+                  vocabSize: Int,
+                  stopwordFile: String): (RDD[(Long, Vector)], Array[String], Long) = {
 
     val sqlContext = SQLContext.getOrCreate(sc)
     import sqlContext.implicits._
 
-    // 获取文本文件的数据集
-    // 在每个文件中每个文本一行。 如果输入目录包含许多小文件，
-    // 将导致非常多的小分区，这些大量的小分区将降低性能。
-    // 在此情况下，考虑使用coalesce() 方法创建更少的、更大的分区。
     val df = sc.textFile(paths.mkString(",")).toDF("docs")
     val customizedStopWords: Array[String] = if (stopwordFile.isEmpty) {
-      Array.empty[String]
+      Array.empty[String]   //用户自定义停用词
     } else {
       val stopWordText = sc.textFile(stopwordFile).collect()
       stopWordText.flatMap(_.stripMargin.split("\\s+"))
@@ -64,29 +67,150 @@ object LDAUtils {
       .map(_.swap)
 
     (documents,
-      model.stages(2).asInstanceOf[CountVectorizerModel].vocabulary,  // vocabulary
-      documents.map(_._2.numActives).sum().toLong) // total token count
+      model.stages(2).asInstanceOf[CountVectorizerModel].vocabulary,  // 词汇表
+      documents.map(_._2.numActives).sum().toLong) // 总词数
   }
 
   /**
-    * 打印 “主题-词” 结果
-    * @param topicsDF 包含主题的DataFrame
-    * @param vocab  词汇表
+    * 打印语料信息
+    * @param corpus 语料
+    * @param vocabArray 词汇表
+    * @param actualNumTokens  tokens个数
     */
-  def printTopics(topicsDF: DataFrame, vocab: Array[String]): Unit = {
-    val topicsRDD = topicsDF.map( x => (x.getInt(0), x.getSeq[Int](1).toArray, x.getSeq[Double](2).toArray))
+  def corpusInfo(corpus: RDD[(Long, Vector)], vocabArray: Array[String], actualNumTokens: Long): Unit = {
+    val actualCorpusSize = corpus.count()
+    val actualVocabSize = vocabArray.length
 
-    val topics = topicsRDD.map { case (topic, terms, termWeights) =>
-      (topic, terms.zip(termWeights).map { case (term, weight) => (vocab(term.toInt), weight) })
-    }.sortByKey(ascending = true)
+    println()
+    println(s"语料信息：")
+    println(s"\t 训练集大小：$actualCorpusSize documents")
+    println(s"\t 词汇表大小：$actualVocabSize terms")
+    println(s"\t 总词汇数： $actualNumTokens tokens")
+    println()
+  }
 
-    println("10 topics:")
-    topics.foreach { case (topicID, topic) =>
-      println(s"TOPIC $topicID")
-      topic.foreach { case (term, weight) =>
-        println(s"$term\t$weight")
-      }
-      println()
+  /**
+    * 训练模型
+    * @param corpus 语料
+    * @param k  topic个数
+    * @param maxIterations  最大迭代次数
+    * @param algorithm  算法（em或online）
+    * @return LDAModel
+    */
+  def train(corpus: RDD[(Long, Vector)], k: Int = 20, maxIterations: Int = 20, algorithm: String = "em"): LDAModel = {
+    val lda = new LDA()
+
+    val actualCorpusSize = corpus.count()
+
+    val optimizer = algorithm.toLowerCase match {
+      case "em" => new EMLDAOptimizer
+      // add (1.0 / actualCorpusSize) to MiniBatchFraction be more robust on tiny datasets.
+      case "online" => new OnlineLDAOptimizer().setMiniBatchFraction(0.05 + 1.0 / actualCorpusSize)
+      case _ => throw new IllegalArgumentException(
+        "目前只支持em和online算法！")
     }
+
+    lda.setOptimizer(optimizer)
+      .setK(k)
+      .setMaxIterations(maxIterations)
+      .setDocConcentration(-1)
+      .setTopicConcentration(-1)
+      .setCheckpointInterval(10)
+
+    val startTime = System.nanoTime()
+    val ldaModel = lda.run(corpus)
+    val elapsed = (System.nanoTime() - startTime) / 1e9
+
+    println("完成模型训练！")
+    println(s"\t 耗时：$elapsed 秒")
+
+    ldaModel
+  }
+
+  /**
+    * 主题描述，包括主题下每个词以及词的权重
+    * @param ldaModel LDAModel
+    * @param vocabArray 词汇表
+    * @return 主题-词结果
+    */
+  def topicWords(ldaModel: LDAModel, vocabArray: Array[String]): Array[Array[(String, Double)]] = {
+    val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = 20)
+    topicIndices.map { case (terms, termWeights) =>
+      terms.zip(termWeights).map { case (term, weight) => (vocabArray(term.toInt), weight) }
+    }
+  }
+
+  /**
+    * 文档-主题分布结果
+ *
+    * @param ldaModel LDAModel
+    * @param corpus  文档
+    * @return “文档-主题分布”：(docID, topicDistributions)
+    */
+  def docTopics(ldaModel: LDAModel, corpus: RDD[(Long, Vector)]): RDD[(Long, Vector)] = {
+    var topicDistributions: RDD[(Long, Vector)] = null
+    ldaModel match {
+      case distLDAModel: DistributedLDAModel =>
+        topicDistributions = distLDAModel.topicDistributions
+      case localLDAModel: LocalLDAModel =>
+        topicDistributions = localLDAModel.topicDistributions(corpus)
+      case _ =>
+    }
+
+    topicDistributions
+  }
+
+  /**
+    * 评估模型，评估标准：似然率和混乱率
+ *
+    * @param corpus  数据
+    * @param ldaModel LDAModel
+    * @return (logLikelihood, logPerplexity)
+    */
+  def evaluation(corpus: RDD[(Long, Vector)], ldaModel: LDAModel): (Double, Double) = {
+    var logLikelihood = 0.0
+    var logPerplexity = 0.0
+
+    ldaModel match {
+      case distLDAModel: DistributedLDAModel =>
+        logLikelihood = distLDAModel.logLikelihood
+        logPerplexity = distLDAModel.logPrior
+      case localLDAModel: LocalLDAModel =>
+        logLikelihood = localLDAModel.logLikelihood(corpus)
+        logPerplexity = localLDAModel.logPerplexity(corpus)
+      case _ =>
+    }
+    (logLikelihood, logPerplexity)
+  }
+
+  /**
+    * 保存模型和词汇表
+    * @param sc SparkContext
+    * @param modelPath  模型保存路径
+    * @param ldaModel LDAModel
+    * @param df 词汇表：(index, word)
+    */
+  def saveModel(sc: SparkContext, modelPath: String, ldaModel: LDAModel, df: DataFrame): Unit = {
+    ldaModel.save(sc, modelPath + File.separator + "model")
+    df.write.parquet(modelPath + File.separator + "df")
+  }
+
+  /**
+    * 预测新文档的“文档-主题分布”
+    * @param ldaModel LDAModel
+    * @param documents  文档
+    * @return “文档-主题分布”：(docID, topicDistributions)
+    */
+  def predict(ldaModel: LDAModel, documents: RDD[(Long, Vector)]): RDD[(Long, Vector)] = {
+    var topicDistributions: RDD[(Long, Vector)] = null
+    ldaModel match {
+      case distLDAModel: DistributedLDAModel =>
+        topicDistributions = distLDAModel.toLocal.topicDistributions(documents)
+      case localLDAModel: LocalLDAModel =>
+        topicDistributions = localLDAModel.topicDistributions(documents)
+      case _ =>
+    }
+
+    topicDistributions
   }
 }
