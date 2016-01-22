aa5af84496ceb11b0a3676eda5abcfbd0f4eb9b6
yhao2014 <https://github.com/>
2016-01-22 16:21:48 +0800

    添加ANSJ分词使用的crf.model

--- ckooc-nlp/src/main/java/segment/AnsjEvaluation.java
+++ ckooc-nlp/src/main/java/segment/AnsjEvaluation.java
@@ -48,16 +48,19 @@ public class AnsjEvaluation implements Segmenter {
     }
 
     public static void main(String[] args)throws IOException {
-        String input = "G:/data/baike/test.txt";
-        String output = "G:/data/baike/test_splited.txt";
+        String input = "G:/test/sample.txt";
+        String output = "G:/result/test_splited.txt";
 
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(input)));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output)));
 
-        String line;
+        /*String line;
         while ((line = br.readLine()) != null) {
-            bw.write(NlpAnalysis.parse(line).toString() + "\n");
-        }
+            bw.write(ToAnalysis.parse(line).toString() + "\n");
+        }*/
+
+        List<Term> parse = NlpAnalysis.parse("洁面仪配合洁面深层清洁毛孔 清洁鼻孔面膜碎觉使劲挤才能出一点点皱纹 脸颊毛孔修复的看不见啦 草莓鼻历史遗留问题没辙 脸和脖子差不多颜色的皮肤才是健康的 长期使用安全健康的比同龄人显小五到十岁 28岁的妹子看看你们的鱼尾纹");
+        System.out.println(parse);
 
         br.close();
         bw.close();
